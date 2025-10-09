d504afb3ae09c200b362ac81578430bbab10b5a0
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-10-09 13:26:39 +0300

    Tehtävä 1.10: Erotettu Button ja StatisticLine omiksi komponenteikseen

--- Osa_1/unicafe/src/App.jsx
+++ Osa_1/unicafe/src/App.jsx
@@ -1,28 +1,35 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = ({ text, value, unit }) => (
+  <p>{text}: {value} {unit}</p>
+);
+
 const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = total === 0 ? 0 : (good - bad) / total;
   const positive = total === 0 ? 0 : (good / total) * 100;
-  
+
   if (total === 0) {
-        return <p>No feedback given</p>;
+    return <p>No feedback given</p>;
   }
-  
+
   return (
     <>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
-      <p>all: {total}</p>
-      <p>average: {average}</p>
-      <p>positive: {positive} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={total} />
+      <StatisticLine text="average" value={average} />
+      <StatisticLine text="positive" value={positive} unit="%" />
     </>
   );
 };
 
 const App = () => {
-  // tilat
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -30,9 +37,9 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
 
       <h2>Statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad} />
