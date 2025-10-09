25240fbf5c3b485100d3b61ed459a326155ee395
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-10-09 12:52:23 +0300

    Tehtävä 1.6: Toteutettu palautepainikkeet ja tilojen seuranta (good, neutral, bad)

---
+++ Osa_1/unicafe/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+
+const App = () => {
+  // tilat
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  // laskut
+  const total = good + neutral + bad;
+  const average = total === 0 ? 0 : (good - bad) / total;
+  const positive = total === 0 ? 0 : (good / total) * 100;
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+
+      <h2>Statistics</h2>
+      {total === 0 ? (
+        <p>No feedback</p>
+      ) : (
+        <>
+          <p>good: {good}</p>
+          <p>neutral: {neutral}</p>
+          <p>bad: {bad}</p>
+        </>
+      )}
+
+    </div>
+  )
+}
+
+export default App
