95b979e5dbbd90330479c0ffcf4c2d817040f027
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-10-20 14:08:13 +0300

    Tehtävä 2.9: Lisätty hakukenttä nimien suodattamiseen case-insensitiivisesti

--- Osa_2/puhelinluettelo/src/App.jsx
+++ Osa_2/puhelinluettelo/src/App.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
 
 const App = () => {
+  const [filter, setFilter] = useState('')
+
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '050 555 7678' }
+    { name: 'Arto Hellas', number: '050-5557678' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
   ]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumeber] = useState('')
@@ -26,10 +30,21 @@ const App = () => {
     setNewNumeber('')  
   }
 
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        />
+      </div>
       <form onSubmit={handleSubmit}>
+        <h2>Add a new</h2>
         <div>
           name: <input
           value={newName}
@@ -48,9 +63,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => (
+        {filteredPersons.map(person => (
           <li key={person.name}>
-            {person.name} - {person.number}
+            {person.name} {person.number}
           </li>
         ))}
       </div>
