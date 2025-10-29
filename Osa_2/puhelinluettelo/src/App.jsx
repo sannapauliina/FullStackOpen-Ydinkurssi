473a76a37f9c8316edda3047025f141d41640661
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-10-29 13:40:19 +0200

    Tehtävä 2.13: Palvelinlogiikka siirretty omaan services/persons.js-moduuliin

--- Osa_2/puhelinluettelo/src/App.jsx
+++ Osa_2/puhelinluettelo/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import personService from './services/persons'
+
 
 const Filter = ({ filter, handleFilterChange }) => (
   <div>
@@ -60,13 +62,13 @@ const App = () => {
 
     const newPerson = { name: newName, number: newNumber }
     
-    axios
-      .post('http://localhost:3001/persons', newPerson)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(newPerson)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
         setNewName('')
         setNewNumber('')
-      }) 
+      })
     }
 
   const filteredPersons = persons.filter(person =>
@@ -74,11 +76,11 @@ const App = () => {
   )
 
   useEffect(() => {
-  axios
-    .get('http://localhost:3001/persons')
-    .then(response => {
-      setPersons(response.data)
-    })
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
   }, [])
 
   return (
