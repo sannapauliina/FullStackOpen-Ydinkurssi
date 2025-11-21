5d7aa2db173f2946aed2c8c0c87909b59562e136
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-11-21 11:47:00 +0200

    Tehtävä 3.7: Lisätty morgan middleware HTTP‑pyyntöjen lokitukseen, konfiguroitu käyttämään tiny‑formaattia

--- Osa_3/puhelinluettelo_backend/index.js
+++ Osa_3/puhelinluettelo_backend/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+
+app.use(morgan('tiny'))
 
 let persons = [
   { id: "1", name: "Arto Hellas", number: "050-12345678" },
