fef1b6c6d1439f523b60c6bf062a94353aa0feb1
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2025-12-11 16:06:12 +0200

    Tehtävä 4.4: Toteutettu totalLikes-apufunktio ja yksikkötestit

--- Osa_4/blogilista/tests/list_helper.test.js
+++ Osa_4/blogilista/tests/list_helper.test.js
@@ -1,6 +1,41 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  const listWithManyBlogs = [
+    { title: 'Blog1', author: 'Author1', url: 'url1', likes: 7 },
+    { title: 'Blog2', author: 'Author2', url: 'url2', likes: 3 },
+    { title: 'Blog3', author: 'Author3', url: 'url3', likes: 12 }
+  ]
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listWithManyBlogs)
+    console.log('Total likes:', result)
+    assert.strictEqual(result, 22)
+  })
+})
 
 test('dummy returns one', () => {
   const blogs = []
