c591348ced3e70042e3d058d5d4d937f46bdafd7
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-01-22 15:54:31 +0200

    Tehtävä 5.10: Lisätty blogilistan lajittelu tykkäysten perusteella

--- Osa_5/bloglist-frontend/src/App.jsx
+++ Osa_5/bloglist-frontend/src/App.jsx
@@ -144,9 +144,14 @@ const App = () => {
       </Togglable>
 
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
-      )}
+      {blogs
+        .slice()
+        .sort((a, b) => b.likes - a.likes)
+        .map(blog =>
+          <Blog key={blog.id} blog={blog} likeBlog={likeBlog} />
+        )
+      }
+
     </div>
   )
 }
