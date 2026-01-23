9c281119ebc5e158c48d657f5a60bc32221afa70
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-01-23 09:34:04 +0200

    Tehtävä 5.14: Lisätty testi view‑napin toiminnalle

--- Osa_5/bloglist-frontend/src/components/Blog.test.jsx
+++ Osa_5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,7 +1,9 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('Blog component', () => {
+
   test('renders title and author but not url or likes by default', () => {
     const blog = {
       title: 'Testing React components',
@@ -19,14 +21,41 @@ describe('Blog component', () => {
     render(<Blog blog={blog} user={user} />)
 
     // näkyvät
-    expect(screen.getByText(/Testing React components/i)).toBeDefined()
+    expect(screen.getByText(/Testing React components/i)).toBeInTheDocument()
     expect(screen.getByText(/Test Author/i)).toBeInTheDocument()
 
     // eivät näy oletuksena
-    const urlElement = screen.queryByText('http://example.com')
-    const likesElement = screen.queryByText('likes 10')
+    expect(screen.queryByText('http://example.com')).toBeNull()
+    expect(screen.queryByText(/likes 10/i)).toBeNull()
+  })
 
-    expect(urlElement).toBeNull()
-    expect(likesElement).toBeNull()
+  test('renders url, likes and user after clicking the view button', async () => {
+    const blog = {
+      title: 'Testing React components',
+      author: 'Test Author',
+      url: 'http://example.com',
+      likes: 10,
+      user: {
+        username: 'tester',
+        name: 'Test User'
+      }
+    }
+
+    const user = { username: 'tester' }
+
+    render(<Blog blog={blog} user={user} />)
+
+    const userSim = userEvent.setup()
+
+    // Etsi view-nappi
+    const button = screen.getByText('view')
+
+    // Klikkaa sitä
+    await userSim.click(button)
+
+    // Nyt url, likes ja user.name pitäisi näkyä
+    expect(screen.getByText('http://example.com')).toBeInTheDocument()
+    expect(screen.getByText(/likes 10/i)).toBeInTheDocument()
+    expect(screen.getByText('Test User')).toBeInTheDocument()
   })
 })
