65aebf27b9d0d31cc72e88823dfb4997f9c1e567
Sanna Hiltunen <sannap.hiltunen@gmail.com>
2026-02-23 11:17:02 +0200

    Tehtävä 7.18: backendin kommenttituki

--- Osa_4/blogilista/controllers/blogs.js
+++ Osa_4/blogilista/controllers/blogs.js
@@ -1,88 +1,103 @@
-const express = require('express')
-const Blog = require('../models/blog')
-const logger = require('../utils/logger')
+const express = require("express");
+const Blog = require("../models/blog");
+const logger = require("../utils/logger");
 
-const User = require('../models/user')
-const blogsRouter = express.Router()
-const middleware = require('../utils/middleware')
+const User = require("../models/user");
+const blogsRouter = express.Router();
+const middleware = require("../utils/middleware");
 
-blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog
-    .find({})
-    .populate('user', { username: 1, name: 1 })
+blogsRouter.get("/", async (req, res) => {
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
-  res.json(blogs)
-})
+  res.json(blogs);
+});
 
-blogsRouter.post('/', middleware.userExtractor, async (req, res, next) => {
-  console.log('USER IN REQUEST:', req.user)
+blogsRouter.post("/", middleware.userExtractor, async (req, res, next) => {
+  console.log("USER IN REQUEST:", req.user);
 
   try {
-    const body = req.body
-    const user = req.user
+    const body = req.body;
+    const user = req.user;
 
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
       likes: body.likes || 0,
-      user: user._id
-    })
+      user: user._id,
+    });
 
-    const savedBlog = await blog.save()
+    const savedBlog = await blog.save();
 
     // lisää blogi käyttäjän listaan
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-    res.status(201).json(savedBlog)
+    res.status(201).json(savedBlog);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-blogsRouter.delete('/:id', middleware.userExtractor, async (req, res, next) => {
+blogsRouter.delete("/:id", middleware.userExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.findById(req.params.id)
+    const blog = await Blog.findById(req.params.id);
 
     if (!blog) {
-      return res.status(404).json({ error: 'blog not found' })
+      return res.status(404).json({ error: "blog not found" });
     }
 
     // Vain lisääjä saa poistaa
     if (blog.user.toString() !== req.user.id.toString()) {
-      return res.status(403).json({ error: 'only the creator can delete a blog' })
+      return res
+        .status(403)
+        .json({ error: "only the creator can delete a blog" });
     }
 
-    await Blog.findByIdAndDelete(req.params.id)
-    return res.status(204).end()
-
+    await Blog.findByIdAndDelete(req.params.id);
+    return res.status(204).end();
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-blogsRouter.put('/:id', async (req, res, next) => {
+blogsRouter.put("/:id", async (req, res, next) => {
   try {
-    const { likes } = req.body
+    const { likes } = req.body;
 
     const updatedBlog = await Blog.findByIdAndUpdate(
       req.params.id,
       { likes },
-      { new: true, runValidators: true, context: 'query' }
-    )
+      { new: true, runValidators: true, context: "query" },
+    );
 
     if (!updatedBlog) {
-      const error = new Error('blog not found')
-      error.status = 404
-      return next(error)
+      const error = new Error("blog not found");
+      error.status = 404;
+      return next(error);
     }
 
-    res.json(updatedBlog)
+    res.json(updatedBlog);
   } catch (error) {
-    error.status = 400
-    next(error)
+    error.status = 400;
+    next(error);
   }
-})
+});
 
-module.exports = blogsRouter
+// kommentit
+blogsRouter.post("/:id/comments", async (request, response) => {
+  const { comment } = request.body;
+
+  const blog = await Blog.findById(request.params.id);
+
+  if (!blog) {
+    return response.status(404).json({ error: "blog not found" });
+  }
+
+  blog.comments = blog.comments.concat(comment);
+  const updatedBlog = await blog.save();
+
+  response.status(201).json(updatedBlog);
+});
+
+module.exports = blogsRouter;
--- Osa_4/blogilista/models/blog.js
+++ Osa_4/blogilista/models/blog.js
@@ -1,32 +1,36 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: true
+    required: true,
   },
   author: String,
   url: {
     type: String,
-    required: true
+    required: true,
   },
   likes: {
     type: Number,
-    default: 0
+    default: 0,
   },
-  user: { 
-    type: mongoose.Schema.Types.ObjectId, 
-    ref: 'User' 
-  }
-})
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  comments: [
+    {
+      type: String,
+    },
+  ],
+});
 
-blogSchema.set('toJSON', { 
-  transform: (document, returnedObject) => { 
-    returnedObject.id = returnedObject._id.toString() 
-    delete returnedObject._id 
-    delete returnedObject.__v 
-  } 
-})
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
 
-module.exports = mongoose.model('Blog', blogSchema)
-
+module.exports = mongoose.model("Blog", blogSchema);
